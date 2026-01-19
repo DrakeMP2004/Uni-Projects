a9d1f2a2de23bee63a41ca4b7860ca673b72968a
DrakeMP2004 <160020478+DrakeMP2004@users.noreply.github.com>
2026-01-19 11:42:42 +0000

    Add files via upload
    An Interactive Food Pyramid made in CSS/HTML/JS

---
+++ Web Development/Assignment 1/assignment-01.js
@@ -0,0 +1,84 @@
+// Add Funtion of Counter1
+function add1()
+{
+    let value = document.getElementById("counter1").innerHTML; //Let value be default set from HTML input
+    value++; //Increment by 1 if '+' button is pressed
+    document.getElementById("counter1").innerHTML = value; //Change default value to incremented value
+}
+// Subtract Funtion of Counter1
+function subtract1()
+{
+    let value = document.getElementById("counter1").innerHTML; //Let value be default set from HTML input
+    value--; //Decrement by 1 if '-' button is pressed
+    document.getElementById("counter1").innerHTML = value; //Change default value to decremented value
+}
+
+// Do the same for the other counters
+function add2()
+{
+    let value = document.getElementById("counter2").innerHTML;
+    value++;
+    document.getElementById("counter2").innerHTML = value;
+}
+function subtract2()
+{
+    let value = document.getElementById("counter2").innerHTML;
+    value--;
+    document.getElementById("counter2").innerHTML = value;
+}
+
+
+function add3()
+{
+    let value = document.getElementById("counter3").innerHTML;
+    value++;
+    document.getElementById("counter3").innerHTML = value;
+}
+function subtract3()
+{
+    let value = document.getElementById("counter3").innerHTML;
+    value--;
+    document.getElementById("counter3").innerHTML = value;
+}
+
+
+function add4()
+{
+    let value = document.getElementById("counter4").innerHTML;
+    value++;
+    document.getElementById("counter4").innerHTML = value;
+}
+function subtract4()
+{
+    let value = document.getElementById("counter4").innerHTML;
+    value--;
+    document.getElementById("counter4").innerHTML = value;
+}
+
+
+function add5()
+{
+    let value = document.getElementById("counter5").innerHTML;
+    value++;
+    document.getElementById("counter5").innerHTML = value;
+}
+function subtract5()
+{
+    let value = document.getElementById("counter5").innerHTML;
+    value--;
+    document.getElementById("counter5").innerHTML = value;
+}
+
+
+function add6()
+{
+    let value = document.getElementById("counter6").innerHTML;
+    value++;
+    document.getElementById("counter6").innerHTML = value;
+}
+function subtract6()
+{
+    let value = document.getElementById("counter6").innerHTML;
+    value--;
+    document.getElementById("counter6").innerHTML = value;
+}
